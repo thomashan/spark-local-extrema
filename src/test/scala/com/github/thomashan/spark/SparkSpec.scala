e304c160e2c5a32755320a64497b1b5cc788792d
Thomas Han <thomas.han@live.com>
2017-10-18 23:03:15 +1100

    pulled up common methods

--- src/test/scala/com/github/thomashan/spark/SparkSpec.scala
+++ src/test/scala/com/github/thomashan/spark/SparkSpec.scala
@@ -1,8 +1,13 @@
 package com.github.thomashan.spark
 
 import com.holdenkarau.spark.testing.{DatasetSuiteBase, SparkSessionProvider}
+import org.apache.spark.sql.DataFrame
 import org.scalatest.{BeforeAndAfter, FunSpec}
 
 abstract class SparkSpec extends FunSpec with DatasetSuiteBase with BeforeAndAfter {
   override lazy implicit val spark = SparkSessionProvider._sparkSession
+
+  protected def loadCsvFile(path: String): DataFrame = {
+    spark.read.option("header", true).option("inferSchema", true).csv(path)
+  }
 }
