775a3e7cde2fb0dc8f3f5aa7d8b2e0597dcc134c
Thomas Han <thomas.han@live.com>
2017-11-05 21:52:18 +1100

    added doc for running docker command

--- src/main/scala/com/github/thomashan/spark/hilow/extrema/CompleteDatasetJob.scala
+++ src/main/scala/com/github/thomashan/spark/hilow/extrema/CompleteDatasetJob.scala
@@ -2,6 +2,10 @@ package com.github.thomashan.spark.hilow.extrema
 
 import com.github.thomashan.spark.SparkJob
 
+//  docker run --rm -it -p 4040:4040 \
+//  -v $(pwd)/examples:/data \
+//  -v $(pwd)/target/scala-2.11/spark-local-extrema-assembly-0.1-SNAPSHOT.jar:/job.jar \
+//  gettyimages/spark bin/spark-submit --master local[*] --driver-memory 2g --class com.github.thomashan.spark.hilow.extrema.CompleteDatasetJob /job.jar /data/hi_low.csv true x hi low /data/hi_low_extremas
 class CompleteDatasetJob extends SparkJob {
   override val applicationName: String = getClass.getName
 
