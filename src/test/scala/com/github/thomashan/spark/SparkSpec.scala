68474403458e7c144b29eab9ed84cad923829c05
Thomas Han <thomas.han@live.com>
2017-12-05 16:55:39 +1100

    set the log to warn for tests

--- src/test/scala/com/github/thomashan/spark/SparkSpec.scala
+++ src/test/scala/com/github/thomashan/spark/SparkSpec.scala
@@ -2,10 +2,18 @@ package com.github.thomashan.spark
 
 import com.holdenkarau.spark.testing.{DatasetSuiteBase, SparkSessionProvider}
 import org.apache.spark.sql.DataFrame
-import org.scalatest.{BeforeAndAfter, FunSpec}
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSpec}
 
-abstract class SparkSpec extends FunSpec with DatasetSuiteBase with BeforeAndAfter {
+abstract class SparkSpec extends FunSpec with DatasetSuiteBase with BeforeAndAfter with BeforeAndAfterAll {
   override lazy implicit val spark = SparkSessionProvider._sparkSession
+
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    spark
+      .sparkContext
+      .setLogLevel("WARN")
+  }
 
   protected def loadCsvFile(path: String): DataFrame = {
     spark.read.option("header", true).option("inferSchema", true).csv(path)
